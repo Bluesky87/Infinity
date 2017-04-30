1a9f3c4d08d54b9dcc9d55a7aaba218366f17661
Grzegorz Kasperek <grzegorz.k@investmag.pl>
2017-04-30 19:52:40 +0200

    mock guzzle

---
+++ app/Guzzle/history.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Grzegorz Kasperek
+ */
+
+require '../../vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+
+$bucket = [];
+$history = Middleware::history($bucket);
+$stack = HandlerStack::create();
+$stack->push($history);
+$client = new Client(['handler' => $stack]);
+
+$client->get('http://jsonplaceholder.typicode.com/posts/2');
+$client->get('http://jsonplaceholder.typicode.com/posts/0', ['http_errors' => false]);
+
+echo count($bucket) . "</br>";
+
+foreach ($bucket as $item) {
+    echo $item['request']->getUri() . "</br>";
+    echo $item['response']->getBody() . "</br>";
+}
